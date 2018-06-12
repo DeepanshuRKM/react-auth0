67f58df0269076cbd1746d192177ed2d2c1a0c8e
Deepanshu Kumar <deepanshu@rkmarketing.net>
2018-06-12 10:47:29 +0530

    Basis Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-auth0
+
+This is task - 4
---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'reactauth0.auth0.com',
+  clientId: 'fZXVUGTHt7CkQRWWoR9498WdgH9VBZuU',
+  callbackUrl: 'http://localhost:3000/callback'
+}
